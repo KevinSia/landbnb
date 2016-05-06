b3a048fb89b51934f20e44a0f1e299868f6fe98e
KevinSia <kevinsiakeemun@gmail.com>
2016-05-06 18:28:48 +0800

    nav bar done

--- Gemfile
+++ Gemfile
@@ -11,32 +11,23 @@ source 'https://rubygems.org'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
-# Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
-# Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-
-# Use jquery as the JavaScript library
 gem 'jquery-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
-# bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
-# Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
-gem 'slim-rails'
 gem 'will_paginate'
 gem 'country_select'
 
-#
+gem 'slim-rails'
+gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass'
+gem 'bootstrap_form'
+
+# application.yml
 gem 'figaro'
 
 # authentication
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,60 @@
 class UsersController < Clearance::UsersController
 
-  before_action :require_login, except: [:show]
-
+  before_action :require_login, except: [:create]
+  before_action :check_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(permit_params)
+      redirect_to user_path(@user)
+    else
+      flash[:warning] = 'Incorrect input(s)'
+      render :edit
+    end
   end
 
   private
 
   def user_from_params
     user_params = params[:user] || Hash.new
-    first_name = user_params.delete(:first_name)
-    last_name = user_params.delete(:last_name)
     email = user_params.delete(:email)
     password = user_params.delete(:password)
+    first_name = user_params.delete(:first_name)
+    last_name = user_params.delete(:last_name)
     gender = user_params.delete(:gender)
-    description = user_params.delete(:description)
+    country = user_params.delete(:country)
     phone_number = user_params.delete(:phone_number)
+    description = user_params.delete(:description)
 
     Clearance.configuration.user_model.new(user_params).tap do |user|
-      user.first_name = first_name
-      user.last_name = last_name
       user.email = email
       user.password = password
+      user.first_name = first_name
+      user.last_name = last_name
       user.gender = gender
-      user.description = description
+      user.country = country
       user.phone_number = phone_number
+      user.description = description
     end
   end
 
   def permit_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :gender, :description,
-                                 :phone_number)
+      :country, :phone_number)
+  end
+
+  def check_user
+    unless User.find(params[:id]) == current_user
+      flash[:warning] = 'Y U DO DIS'
+      redirect_to '/'
+    end
   end
 end
